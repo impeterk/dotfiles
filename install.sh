c418bcffd6bcc8cd3ab5657f4148f6444a9e0fd0
peter <peter@works7ation.com>
2023-09-10 21:03:34 +0200

    added emacs /doomemacs

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@
 #
 ################################################################################
 
-#ln -rs ~/.dotfiles/nvim ~/.config/nvim
-#ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-cp ~/.dotfiles/.fancy-bash-prompt.sh ~/.fancy-bash-prompt.sh
-echo "source ~/.fancy-bash-prompt.sh" >> ~/.bashrc
+ln -rs ~/.dotfiles/nvim ~/.config/nvim
+ln -rs ~/.dotfiles/doom ~/.config/doom
+ln -rs ~/.dotfiles/emacs ~/.config/emacs
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+#cp ~/.dotfiles/.fancy-bash-prompt.sh ~/.fancy-bash-prompt.sh
+#echo "source ~/.fancy-bash-prompt.sh" >> ~/.bashrc
