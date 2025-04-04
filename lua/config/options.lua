426d5b114a1f46e3073dcdf63c1a1c5f85acde24
peter kudelas <peterkudelas@gmail.com>
2025-04-04 16:29:01 +0200

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = false
+local opt = vim.opt
+opt.cursorline = false
+opt.scrolloff = 22
